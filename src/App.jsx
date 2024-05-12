826110051fde120f9bb038583887341c476c3c1f
Sourav <garaisourav143@gmail.com>
2024-05-12 16:14:54 +0530

    api working

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import NewsBoard from "./components/NewsBoard";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <NewsBoard></NewsBoard>
+    </div>
+  );
 }
 
 export default App;
